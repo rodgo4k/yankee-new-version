8d6e7677c29ac468949358599917862b59ca3c35
Rodrigo <rodrigoaprestes@gmail.com>
2026-07-25 18:11:15 -0300

    nova att

--- src/components/AnimatedSection.tsx
+++ src/components/AnimatedSection.tsx
@@ -28,7 +28,7 @@ const AnimatedSection = ({
       whileInView={{ opacity: 1, y: 0, x: 0 }}
       viewport={{ once: true, margin: "-40px" }}
       transition={{
-        duration: 0.7,
+        duration: 1.05,
         delay,
         ease: [0.25, 0.46, 0.45, 0.94],
       }}
